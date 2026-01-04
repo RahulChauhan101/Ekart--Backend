b227d10e0301786b54d5093f4dfb07333f1766db
RahulChauhan101 <rahul.chauhan12241@gmail.com>
2026-01-04 15:08:11 +0530

    4/1/2025

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();   
+import cors from "cors";
+
+dotenv.config();
 import connectDB from "./database/db.js";
 import userRoutes from "./database/controllers/routes/userRoutes.js";
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// ✅ VERY IMPORTANT
+app.use(cors({
+  origin: "http://localhost:5173", // React/Vite frontend
+  credentials: true
+}));
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
